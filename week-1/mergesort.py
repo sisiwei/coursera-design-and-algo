3633ff92c3e3fa9436c4159c443ce781e22b49c5
Sisi Wei <me@sisiwei.com>
2012-06-13 02:05:57 -0400

    First draft of mergesort - does not account for odd numbered arrays or equal values

---
+++ week-1/mergesort.py
@@ -0,0 +1,33 @@
+array = [5,8,1,3,2,4,9,7,10,15,12,13,11,14,16]
+
+def mergeSort(input):
+	if len(input) == 1:
+		return input	
+	else:
+		halfPoint = len(input)/2
+		firstHalf = mergeSort(input[0:halfPoint])
+		secondHalf = mergeSort(input[halfPoint:])
+		i = 0
+		j = 0
+		output = []
+		
+		while len(output) != len(input):
+			if i == halfPoint:
+				output.append(secondHalf[j])
+				j = j + 1
+			elif j == halfPoint:
+				output.append(firstHalf[i])
+				i = i + 1
+			else:				
+				if firstHalf[i] < secondHalf[j]:
+					output.append(firstHalf[i])
+					i = i + 1
+				elif secondHalf[j] < firstHalf[i]:
+					output.append(secondHalf[j])
+					j = j + 1
+					
+		print output
+		return output
+		
+
+mergeSort(array)
